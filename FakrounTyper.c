36f7de38646b12f94e0f8efd411c7f0c0b73d15c
AMRI Amine <40145608+amine177@users.noreply.github.com>
2018-08-14 21:39:42 +0100

    Aesthetic correction

--- FakrounTyper.c
+++ FakrounTyper.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
 #define error "ma7abbech"
 #define BuffSize 101
 #define border Fakroun("\t\t-----------------------------------\n\n");
@@ -9,26 +10,28 @@
 char *GetText(char *fname);
 void Fakroun(char *text);
 
-void main() {
+int main() {
 	char *file = malloc(10);
 	Fakroun("\n\n\t\t Welcome to Fakroun Typer :p \n");
 	puts("\n");
 	start : Fakroun("\t\t please provide a Filename : ");
 	fgets(file , 10 , stdin);
 	file[strlen(file)-1] = '\0';
-	if( strcmp(GetText(file),error) == 0) {
+	if(!strcmp(GetText(file),error)) {
 		Fakroun(" \t\t Something is wrong with that file ! let's try again \n\n");
 		goto start;
 	}
 	border;
 	Fakroun(GetText(file));
 	border;
+	
+	//Unix systems expect an int return
+	return 0;
 }
 
 void Fakroun(char *text) {
-	int i=0;
-	for( ; i < strlen(text) ; i++) {
-		printf("%c",*(text+i));
+	while (*text) {
+		prutchar(*text++);
 		usleep(90000);
 		fflush(stdout);
 	}
@@ -39,15 +42,14 @@ char *GetText(char *fname) {
 	char *value 	= malloc(500000);
 	char *buffer 	= malloc(BuffSize);
 	if ( fd == NULL) {
-	free(value);
-	return error;
+		free(value);
+		return error;
 	}
 	
-	while ( fgets(buffer , BuffSize , fd ) ) {
+	while ( fgets(buffer , BuffSize , fd ) )
 		strcat(value , buffer);
-	}
 	
 	free(buffer);
-	strcat(value , "\0"); // just to make sure , allright !
+	strcat(value , ""); // just to make sure , allright !
 	return value;
 }
